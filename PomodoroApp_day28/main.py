98794276d5f4a5f52925aa7f80b0aa97999dced7
sapling <sirssapling@gmail.com>
2023-07-05 06:37:56 +0300

    some new features added and some configs made

--- PomodoroApp_day28/main.py
+++ PomodoroApp_day28/main.py
@@ -1,7 +1,8 @@
 import tkinter
 import time
+from tkinter import messagebox
 from totalWorkDays import howManyReps
-
+from PIL import ImageTk, Image   
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,9 +10,11 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 0.1
-SHORT_BREAK_MIN = 0.1
-LONG_BREAK_MIN = 0.1
+FONT_LABEL=('calibre',10, 'bold')
+
+DEFAULT_WORK = 30
+DEFAULT_SHORT_BREAK_MIN = 3
+DEFAULT_LONG_BREAK_MIN = 5
 reps = 0
 timer = None
 
@@ -20,19 +23,19 @@ timer = None
 
 # ---------------------------- UI SETUP ------------------------------- #
 
-window = tkinter.Tk()
-window.title("POMODORO")
+main_window = tkinter.Tk()
+main_window.title("POMODORO")
 #geometry function doesnt limit the min size you can make it as small as you want
 # if you want to set a minsize then use min size method
-#window.geometry("420x300")
-window.minsize(width=350, height=350)
-window.configure(padx=30, pady=30, background=YELLOW)
+#main_window.geometry("420x300")
+main_window.minsize(width=750, height=500)
+main_window.configure(padx=30, pady=30, background=YELLOW)
 
 #Canvas Timer text
-canvas = tkinter.Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)  # to remove border highlighthickness
-tomato = tkinter.PhotoImage(file="tomato.png")
-canvas.create_image(100, 112, image=tomato) #
-timer_txt = canvas.create_text(100,140, text="30:00", fill="white", font=("Arial", 40,'bold'))
+canvas = tkinter.Canvas(width=400, height=450, bg=YELLOW, highlightthickness=0)  # to remove border highlighthickness
+tomato = tkinter.PhotoImage(file="tomato.resized.png")
+canvas.create_image(200, 212, image=tomato) #
+timer_txt = canvas.create_text(200,240, text="30:00", fill="white", font=("Arial", 40,'bold'))
 canvas.grid(row=1, column=1)
 
 # Timer Label
@@ -53,12 +56,64 @@ reset_btn = tkinter.Button(text="Reset",font=("Arial", 7, 'bold'), bg="white")
 reset_btn.config(width=8, height=4)
 reset_btn.grid(row=2, column=2)
 
-
-
-
 #check mark label
 check_mark = tkinter.Label(bg=GREEN, font=("Arial",8))
 check_mark.grid(row=2, column=1)
+
+
+
+#------------------------------------Open A New Window For Timer Configuration---------------#
+
+total_time_tuple = ()
+
+# an image for new window
+#img_config = tkinter.PhotoImage(file="config.png")
+img_config = Image.open("config.png")
+    
+def Config():
+    new_window = tkinter.Toplevel(master=main_window)
+    new_window.title("Configuration")
+    new_window.config(background=GREEN)
+    new_window.minsize(width=800, height=800)
+    
+    work_label = tkinter.Label(new_window, text="Work Time", width=15,background=PINK, font=FONT_LABEL)
+    work_label.grid(row=0,column=0)
+    work_entry = tkinter.Entry(new_window ,width=15)
+    work_entry.grid(row=0, column=1)
+
+    long_break_label = tkinter.Label(new_window, text="Long Break Time", width=15, font=FONT_LABEL, bg=PINK)
+    long_break_label.grid(row=1,column=0)
+    long_break_entry = tkinter.Entry(new_window,textvariable="Enter short long duration in minutes: ", width=15)
+    long_break_entry.grid(row=1, column=1)
+
+    short_break_label = tkinter.Label(new_window, text="Short Break Time", width=15, font=FONT_LABEL, bg=PINK)
+    short_break_label.grid(row=2,column=0)
+    short_break_entry = tkinter.Entry(new_window,textvariable="Enter short break duration in minutes: ", width=15)
+    short_break_entry.grid(row=2, column=1)
+
+    #-------------Configuration Image --------#
+    test = ImageTk.PhotoImage(img_config)
+
+    label1 = tkinter.Label(new_window,image=test, highlightthickness=0)
+    label1.image = test
+
+    # Position image
+    #label1.place(x=0, y=-40)
+    label1.grid(row=5, column=0, columnspan=3, padx=40, pady=50)
+
+    def Ok():
+        global total_time_tuple
+        work_session = int(work_entry.get())
+        long_break_session= int(long_break_entry.get())
+        short_break_session = int(short_break_entry.get())
+        total_time_tuple = (work_session, long_break_session, short_break_session)
+        
+        new_window.destroy() # save and return
+
+    button_accept = tkinter.Button(new_window, background=RED, width=10, text="OK", command=Ok)
+    button_accept.grid(row=1, column=2)
+
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -75,7 +130,7 @@ def countDown(total_time):
     canvas.itemconfig(timer_txt, text=f"{min}:{seconds}")
     if total_time> 0:
         global timer
-        timer = window.after(1000, countDown, total_time)
+        timer = main_window.after(1000, countDown, total_time)
     else:
         start_timer()
 
@@ -83,9 +138,15 @@ def countDown(total_time):
 def start_timer():
     global reps
     reps += 1
-    work_session = WORK_MIN * 60
-    short_break_session = SHORT_BREAK_MIN* 60
-    long_break_session = LONG_BREAK_MIN *60
+    if total_time_tuple == (): # if user goes with the default values
+        work_session = DEFAULT_WORK* 60
+        short_break_session = DEFAULT_SHORT_BREAK_MIN* 60
+        long_break_session = DEFAULT_LONG_BREAK_MIN*60
+    else:
+        work_session = total_time_tuple[0] * 60
+        short_break_session = total_time_tuple[1]* 60
+        long_break_session = total_time_tuple[2]*60
+
     if reps % 8 == 0:
         timer_label.config(text="FOCUS", font=("Arial", 30, 'bold'), fg="RED")
         countDown(long_break_session)
@@ -99,38 +160,36 @@ def start_timer():
         for i in range(int(reps/2)):
             textCheck += '✔'
         check_mark.configure(text= textCheck)
-
-
     start_btn.configure(state="disabled")
     reset_btn.configure(state="active")
 
+
 def reset():
     global  reps
-    window.after_cancel(timer)
+    main_window.after_cancel(timer)
     canvas.itemconfig(timer_txt, text="00:00")
     check_mark.configure(text="")
     timer_label.configure(text="Timer")
     reset_btn.configure(state="disabled")
     start_btn.configure(state="active")
-    howManyReps(int(reps)/2, WORK_MIN, SHORT_BREAK_MIN)
+    if total_time_tuple == ():
+        howManyReps(int(reps)/2, DEFAULT_WORK, DEFAULT_SHORT_BREAK_MIN)
+    else:
+        howManyReps(int(reps)/2, total_time_tuple[0], total_time_tuple[1])
     reps = 0
+
+
+button_config = tkinter.Button(background='white',text="Configure", width=15, command=Config)
+button_config.grid(row=3, column=1)
+
+
+
+messagebox.showinfo(title="Notification", message="Current Work, short and long break time as following:30,3,5\nIn order to "
+                    "change the values please click on the configure button 'Before' starting to a new session")
 
 
 start_btn.configure(command=start_timer)
 reset_btn.configure(command=reset)
 
-#countDown(total_time)
 
-
-
-
-
-
-
-
-
-
-
-
-
-window.mainloop()
+main_window.mainloop()
